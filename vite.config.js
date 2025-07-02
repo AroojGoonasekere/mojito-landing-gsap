075573342b5feb664443b9b51b527742564593e6
AroojGoonasekere <arooj_goonasekere@outlook.com>
2025-07-02 16:41:03 +0530

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/mojito-landing-gsap/', // <-- Add this for GitHub Pages deployment
   plugins: [react(), tailwindcss()],
 })
